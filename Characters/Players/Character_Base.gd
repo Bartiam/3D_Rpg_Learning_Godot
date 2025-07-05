24e9857fc8d86e4bf86e3229d641e9c2f648cd02
Bartiam <leswar@yandex.ru>
2025-07-05 19:05:24 +0500

    Updated.

--- Characters/Players/Character_Base.gd
+++ Characters/Players/Character_Base.gd
@@ -1,10 +1,39 @@
 extends CharacterBody3D
 
+# Variables #
 @export_category("Character Specifications")
-@export var MovementSpeed: float = 5
-@export var JumpVelocity: float = 5
+@export var MovementSpeed: float = 0
+@export var JumpVelocity: float = 0
+@export var MouseSensitivity: float = 0.05
+@export var MinBoundary: float = -60
+@export var MaxBoundary: float = 10
 
 var Gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+# Stores the x/y directions the player is trying to look in
+var LookingCharacter: Vector2 = Vector2.ZERO
+
+@onready var HorisontalPivot: Node3D = $HorisontalPivot
+@onready var VerticalPivot: Node3D = $HorisontalPivot/VerticalPivot
+# Variables #
+
+# Functions #
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	
+
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("Cancel"):
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	
+	if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+		if event is InputEventMouseMotion:
+			LookingCharacter = -event.relative * MouseSensitivity
+			print(LookingCharacter)
+
+
 
 func _physics_process(delta: float) -> void:
 	# add gravity to the character
@@ -14,14 +43,29 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = JumpVelocity
 	
-	var InputDir: Vector2 = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var InputDir: Vector2 = Input.get_vector("Move_Left", "Move_Right", "Move_Forward", "Move_Backward")
 	var Direction: Vector3 = (transform.basis * Vector3(InputDir.x, 0, InputDir.y)).normalized()
 	
 	if (Direction):
 		velocity.x = Direction.x * MovementSpeed
-		velocity.y = Direction.y * MovementSpeed
+		velocity.z = Direction.z * MovementSpeed
 	else:
 		velocity.x = move_toward(velocity.x, 0, MovementSpeed)
 		velocity.z = move_toward(velocity.z, 0, MovementSpeed)
 	
+	frame_camera_rotation(delta)
 	move_and_slide()
+
+
+
+func frame_camera_rotation(DeltaTime: float) -> void:
+	HorisontalPivot.rotate_y(LookingCharacter.x * MouseSensitivity)
+	VerticalPivot.rotate_x(LookingCharacter.y * MouseSensitivity)
+	
+	VerticalPivot.rotation.x = clampf(
+		VerticalPivot.rotation.x,
+		 deg_to_rad(MinBoundary),
+		 deg_to_rad(MaxBoundary))
+	
+	$SpringArm3D.global_transform = VerticalPivot.global_transform
+	LookingCharacter = Vector2.ZERO
